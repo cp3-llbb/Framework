19cdf7d1badc2d7ee12690bc4a1bd93440210c62
mdelcourt <martin.delcourt@uclouvain.be>
2018-02-26 16:45:04 +0100

    Updated framework for CMSSW_9_3_6patch2
    * Removed KaMuCa/Calibration
    * Removed 2016 recipes in bootstrap and postbuild
    * Changed CMSSW Release and arch

--- interface/Tools.h
+++ interface/Tools.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <string>
 
 namespace pat {
     class Jet;
--- plugins/CorrectedMuonProducer.cc
+++ plugins/CorrectedMuonProducer.cc
@@ -3,13 +3,8 @@
 #include <DataFormats/MuonReco/interface/Muon.h>
 #include <DataFormats/PatCandidates/interface/Muon.h>
 
-typedef CorrectedMuonProducerT<reco::Muon, cp3::KaMuCaCorrector> KaMuCaCorrectedMuonProducer;
-typedef CorrectedMuonProducerT<pat::Muon, cp3::KaMuCaCorrector> KaMuCaCorrectedPATMuonProducer;
-
 typedef CorrectedMuonProducerT<pat::Muon, cp3::RochesterCorrector> RochesterCorrectedPATMuonProducer;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-DEFINE_FWK_MODULE(KaMuCaCorrectedMuonProducer);
-DEFINE_FWK_MODULE(KaMuCaCorrectedPATMuonProducer);
 DEFINE_FWK_MODULE(RochesterCorrectedPATMuonProducer);
