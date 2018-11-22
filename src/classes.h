dde3a50491f0e3fd7a5cc1c546b38c04922dd0ef
Claudio Caputo <claudio.caputo@cern.ch>
2018-11-22 17:51:47 +0100

    Add tau gen-truth and tauID map
    - Htautau gen-truth convetion added
    - methods for matching and evaluating the gen-truth added
    - unordered_map to hold all tauID discriminats available added
    - tauID discriminators branches commented
    - tau dz variable added

--- src/classes.h
+++ src/classes.h
@@ -1,9 +1,8 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <unordered_map>
 #include <Math/Vector4D.h>
-#include "cp3_llbb/Framework/interface/GenInfoAndWeights.h"
-#include "DataFormats/Common/interface/Wrapper.h"
 
 namespace {
     struct dictionary {
@@ -15,7 +14,6 @@ namespace {
         std::pair<std::string, bool> dummy6;
         std::vector<std::vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<float>>>> dummy7;
         std::vector<std::vector<std::string>> dummy8;
-        Framework::GenInfoAndWeights dummy9;
-        edm::Wrapper<Framework::GenInfoAndWeights> dummy10;
+	std::vector<std::unordered_map<std::string,float>> dummy9;
     };
 }
